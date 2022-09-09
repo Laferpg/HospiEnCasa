54173ca7ba551cb8fc8a6c2ac568a7f6f8ac96b5
Laferpg <112596190+Laferpg@users.noreply.github.com>
2022-09-08 22:32:24 -0500

    Add files via upload

---
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+using System;
+namespace HospiEnCasa.App.Dominio
+{
+    public class Persona
+    {
+        public int Id {get;set;}
+      /// Cada uno de los atributos de la clase persona
+        public string Nombre{get;set;}
+        
+        public string Apellidos{get;set;}
+        
+        public string NumeroTelefono{get;set;}
+        
+        public Genero Genero{get;set;}
+    }
+}
